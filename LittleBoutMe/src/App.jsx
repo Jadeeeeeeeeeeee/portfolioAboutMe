47010033c45f2543c7cb2662d037a88120db57a6
jadeeeeeeeeeeee <yali.shlomi@icloud.com>
2024-03-03 13:05:38 +0200

    adding files

---
+++ LittleBoutMe/src/App.jsx
@@ -0,0 +1,15 @@
+import SideBar from "./SideBar";
+import LittleBoutMe from "./LittleAboutMe";
+
+function App() {
+
+
+  return(
+    <>
+    <LittleBoutMe/>
+    <SideBar/>
+    </>
+  );
+}
+
+export default App
---
+++ LittleBoutMe/src/LittleAboutMe.jsx
@@ -0,0 +1,19 @@
+
+
+function LittleBoutMe() {
+    return(<>
+    <div className="MainLittleBoutMe">
+        <h1>About me</h1>
+        <p>hello im jadeYeti a -18 web dev proggraming in js html css and using react, i am also learning c# for unity whice is very fun, i started proggraming when i was 10 and ever since that ive been learning new languages and making more projects.</p>
+        <p>some games i like: rainbow six siege , scp sl , csgo, asseto corsa, car x and jedi fallen order</p>
+        <p>some hobbys i like: proggraming , playing video games and playing the electric guitar</p>
+        <p>my favorite animal: cat</p>
+        <h3>some stuff i hate</h3>
+        <p>e daters:i dont mind if you e date just you dont have the say it to fucking everyone</p>
+        <p>stupid questions: theres no need to explain</p>
+        <p>people that try and manipulate other people: also no need to explain </p>
+    </div>
+    </>);
+}
+
+export default LittleBoutMe
